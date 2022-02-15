192baa514038563f1afb3f00cbe6c0a0ead32e9c
Erick <javillatoro1@gmail.com>
2022-02-14 18:32:23 -0600

    initegration with bootstrap

---
+++ frontend/calculator/src/App.js
@@ -0,0 +1,16 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center">
+        <div>
+          <h1 className="text-white col">Calculator</h1>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
