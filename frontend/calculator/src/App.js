af311ed89c476a6c791e3d98611489ee0f704408
Erick <javillatoro1@gmail.com>
2022-02-14 19:33:24 -0600

    Calculator design

--- frontend/calculator/src/App.js
+++ frontend/calculator/src/App.js
@@ -1,14 +1,19 @@
- import './App.css';
+import './App.css';
+import { Calculator } from './Components/Calculator';
 
 function App() {
   return (
-    <div className="container">
-      <div className="d-flex justify-content-center">
-        <div>
-          <h1 className="text-white col">Calculator</h1>
-        </div>
+    <div className="align-items-center
+       container 
+       content 
+       d-flex 
+       flex-column 
+       justify-content-evenly">
+      <div className="col text-center">
+        <h1 className="text-white col">Calculator</h1>
       </div>
-      
+
+      <Calculator />
     </div>
   );
 }
--- frontend/calculator/src/Components/Calculator.js
+++ frontend/calculator/src/Components/Calculator.js
@@ -1,7 +1,74 @@
-import React from 'react'
+import React from 'react';
+
+import './Calculator.css';
 
 export const Calculator = () => {
   return (
-    <div>Calculator</div>
+    <div 
+        className='bg-dark calculator container row'>
+        <div className='row'>
+            <div className='col button text-end hover'>
+                0
+            </div>
+        </div>
+        <div className='row bg-danger' style={{margin:'auto'}}>
+            <button
+                className='col btn btn-danger button'
+            >/</button>
+            <button
+                className='col btn btn-danger button'
+            >*</button>
+            <button
+                className='col btn btn-danger button'
+            >+</button>
+            <button
+                className='col btn btn-danger button'
+            >-</button>
+        </div>
+        <div className='row'  style={{margin:'auto'}}>
+            <button
+                className='col btn btn-dark button'
+            >1</button>
+            <button
+                className='col btn btn-dark button'
+            >2</button>
+            <button
+                className='col btn btn-dark button'
+            >3</button>
+        </div>
+        <div className='row'  style={{margin:'auto'}}>
+            <button
+                className='col btn btn-dark button'
+            >4</button>
+            <button
+                className='col btn btn-dark button'
+            >5</button>
+            <button
+                className='col btn btn-dark button'
+            >6</button>
+        </div>
+        <div className='row'  style={{margin:'auto'}}>
+            <button
+                className='col btn btn-dark button'
+            >7</button>
+            <button
+                className='col btn btn-dark button'
+            >8</button>
+            <button
+                className='col btn btn-dark button'
+            >9</button>
+        </div>
+        <div className='row'  style={{margin:'auto'}}>
+            <button
+                className='col btn btn-dark button'
+            >0</button>
+            <button
+                className='col btn btn-dark button'
+            >.</button>
+            <button
+                className='col btn btn-dark button'
+            >=</button>
+        </div>
+    </div>
   )
 }
